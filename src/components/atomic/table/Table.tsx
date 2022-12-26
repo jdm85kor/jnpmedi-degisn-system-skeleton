f86a4734e6bb80abf172adeec9e8156691bb162b
Dongmin Jang <jdm85kor@gmail.com>
2022-12-26 14:23:49 +0900

    add table

---
+++ src/components/atomic/table/Table.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import { Col } from "./Col";
+import { Row } from "./Row";
+
+const StyledTable = styled.table`
+  position: relative;
+  width: 100%;
+`;
+
+interface Props {
+  head: { title: string; node: React.ReactNode };
+  body: any;
+  children: React.ReactNode;
+}
+
+export const Table = ({ head, body, children }: Props) => {
+  return (
+    <StyledTable>
+      <thead>
+        <Row theme="primary">
+          <th>{head?.title || ""}</th>
+        </Row>
+      </thead>
+      <tbody>
+        {body?.rows?.length &&
+          body.rows.map((row, rowIdx) => {
+            return (
+              <Row theme={row.theme} key={`row-${rowIdx}`}>
+                {row.cols.map((col, colIdx) => {
+                  return (
+                    <Col
+                      theme={col.theme}
+                      key={`col-${colIdx}`}
+                      handler={col.handler}
+                    >
+                      {col.content}
+                    </Col>
+                  );
+                })}
+              </Row>
+            );
+          })}
+        {children}
+      </tbody>
+    </StyledTable>
+  );
+};
